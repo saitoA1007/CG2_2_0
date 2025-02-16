22e6dff35e67c38823f9372ca0c49c2f73d7f484
Aoi Saitou <k024g0004@g.neec.ac.jp>
2025-02-16 15:59:11 +0900

    gitの動作確認

---
+++ Object3d.PS.hlsl
@@ -0,0 +1,12 @@
+
+struct PixelShaderOutput
+{
+    float32_t4 color : SV_TARGER0;
+};
+
+PixelShaderOutput main()
+{
+    PixelShaderOutput output;
+    output.color = float32_t4(1.0, 1.0, 1.0, 1.0);
+    return output;
+}
