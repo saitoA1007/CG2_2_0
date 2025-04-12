2d54558757a063d7ee1466a0fd46b68248e6d812
Aoi Saitou <k024g0004@g.neec.ac.jp>
2025-04-12 20:03:26 +0900

    HalfLambert

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -25,7 +25,10 @@ PixelShaderOutput main(VertexShaderOutput input){
     float32_t4 textureColor = gTexture.Sample(gSampler, input.texcoord);
     
     if (gMaterial.enableLighting){// Lightingする場合
-        float cos = saturate(dot(normalize(input.normal), -gDirectionalLight.direction));
+        
+        // half lambert
+        float NdotL = dot(normalize(input.normal), -gDirectionalLight.direction);
+        float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
         output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
     }else{// Lighttingしない場合。
         output.color = gMaterial.color * textureColor;
