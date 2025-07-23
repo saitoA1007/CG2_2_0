4df6f1953cffbeaec61aa8b94e132ecfeab5681b
saitoA1007 <k024g0004@g.neec.ac.jp>
2025-07-23 23:37:21 +0900

    エンジンの初期化周りを整理

--- EngineSource/Core/GameEngine.h
+++ EngineSource/Core/GameEngine.h
@@ -2,6 +2,7 @@
 #include"EngineSource/Core/WindowsApp.h"
 #include"EngineSource/Core/DirectXCommon.h"
 #include"EngineSource/Core/TextureManager.h"
+
 #include"EngineSource/Core/PSO/LinePSO.h"
 #include"EngineSource/Core/PSO/TrianglePSO.h"
 #include"EngineSource/Core/PSO/ParticlePSO.h"
@@ -9,9 +10,7 @@
 #include"EngineSource/Core/PSO/BloomPSO.h"
 #include"EngineSource/Core/PSO/CopyPSO.h"
 #include"EngineSource/Core/PSO/SpritePSO.h"
-
 #include"EngineSource/Core/PSO/GridPSO.h"
-#include"EngineSource/Core/PSO/GaussianBlurPSO.h"
 
 #include"EngineSource/Core/PostProcess/PostEffectManager.h"
 
@@ -38,6 +37,116 @@
 
 #include"EngineSource/Audio/AudioManager.h"
 
-#pragma comment(lib,"d3d12.lib")
-#pragma comment(lib,"dxgi.lib")
-#pragma comment(lib,"dxguid.lib")
+namespace GameEngine {
+
+	class Engine final {
+	public:
+
+		/// <summary>
+		/// エンジンの初期化
+		/// </summary>
+		/// <param name="title"></param>
+		/// <param name="width"></param>
+		/// <param name="height"></param>
+		void Initialize(const std::wstring& title, const uint32_t& width, const uint32_t& height, HINSTANCE hInstance);
+
+		/// <summary>
+		/// 更新前処理
+		/// </summary>
+		void PreUpdate();
+
+		/// <summary>
+		/// 更新後処理
+		/// </summary>
+		void PostUpdate();
+
+		/// <summary>
+		/// 描画前処理
+		/// </summary>
+		void PreDraw();
+
+		/// <summary>
+		/// 描画後処理
+		/// </summary>
+		void PostDraw();
+
+		/// <summary>
+		/// エンジンの終了処理
+		/// </summary>
+		void Finalize();
+
+		/// <summary>
+		/// ウィンドウが開いているかを判断する
+		/// </summary>
+		/// <returns></returns>
+		bool IsWindowOpen();
+
+	public:
+
+		/// 2D ===========================================
+
+		// ImGuiの機能
+		std::unique_ptr<ImGuiManager> imGuiManager_;
+
+
+		/// Audio ========================================
+
+		// 音声機能
+		std::unique_ptr<AudioManager> audioManager_;
+
+		/// Common =======================================
+
+		// ログ
+		std::unique_ptr<LogManager> logManager_;
+
+		/// Core ========================================
+
+		// Windowsのアプリ機能
+		std::unique_ptr<WindowsApp> windowsApp_;
+
+		// DirectXのアプリ機能
+		std::unique_ptr<DirectXCommon> dxCommon_;
+
+		// dxcCompilerの機能
+		std::unique_ptr<DXC> dxc_;
+
+		// テクスチャの機能
+		std::shared_ptr<TextureManager> textureManager_;
+
+		/// PSO ======================================
+
+		// 通常のPSO設定
+		std::unique_ptr<TrianglePSO> trianglePSO_;
+
+		// パーティクル(複数描画)のPSO設定
+		std::unique_ptr<ParticlePSO> particlePSO_;
+
+		// 線のPSO設定
+		std::unique_ptr<LinePSO> linePSO_;
+
+		// デバック用グリッドのPSO設定
+		std::unique_ptr<GridPSO> gridPSO_;
+
+		// スプライトのPSO設定
+		std::unique_ptr<SpritePSO> spritePSO_;
+
+		/// ↓ポストエフェクト用のPSO設定
+
+		// ポストエフェクトの内容をSwapChainに持ってくるためのPSO設定
+		std::unique_ptr<CopyPSO> copyPSO_;
+
+		// ブルーム用のPSO設定
+		std::unique_ptr<BloomPSO> bloomPSO_;
+
+		/// Input =============================
+
+		std::unique_ptr<Input> input_;
+
+	private:
+
+		/// <summary>
+		/// PSOを作成
+		/// </summary>
+		void CreatePSO();
+	};
+}
