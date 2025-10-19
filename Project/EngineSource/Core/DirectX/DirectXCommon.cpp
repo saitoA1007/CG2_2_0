f78e869a7262435c13b84fe26cd10282ce87cd15
saitoA1007 <k024g0004@g.neec.ac.jp>
2025-10-19 16:09:08 +0900

    60FPSで固定できるようにした

--- Project/EngineSource/Core/DirectX/DirectXCommon.cpp
+++ Project/EngineSource/Core/DirectX/DirectXCommon.cpp
@@ -13,7 +13,10 @@ void DirectXCommon::Initialize(HWND hwnd, uint32_t width, uint32_t height, SrvMa
 
     // 初期化を開始するログ
     LogManager::GetInstance().Log("DirectXCommon Class start Initialize");
-    
+
+    // FPS固定初期化
+    InitializeFixFPS();
+
     // デバイスを生成
     device_ = std::make_unique<DXDevice>();
     device_->Initialize();
@@ -138,6 +141,9 @@ void DirectXCommon::PostDraw(ImGuiManager* imGuiManager)
     // GPUを待つ
     fence_->WaitForGPU(command_->GetQueue());
 
+    // FPS固定
+    UpdateFixFPS();
+
     // 次のフレーム用にコマンドリストを準備
     command_->Reset();
 }
@@ -173,3 +179,31 @@ void DirectXCommon::DebugLayer(ID3D12Device* device) {
     }
 }
 #endif
+
+void DirectXCommon::InitializeFixFPS() {
+    // 現在の時間を記録する
+    reference_ = std::chrono::steady_clock::now();
+}
+
+void DirectXCommon::UpdateFixFPS() {
+    // 1/60秒ピッタリの時間
+    const std::chrono::microseconds kMinTime(uint64_t(1000000.0f / 60.0f));
+    // 1/60秒よりわずかに短い時間
+    const std::chrono::microseconds kMinCheckTime(uint64_t(1000000.0f / 65.0f));
+
+    // 現在の時間を取得する
+    std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
+    // 前回記録からの経過時間を取得する
+    std::chrono::microseconds elapsed = std::chrono::duration_cast<std::chrono::microseconds>(now - reference_);
+
+    // 1/60秒立っていない場合
+    if (elapsed < kMinCheckTime) {
+        // 1/60秒経過するまで微小なスリープを繰り返す
+        while (std::chrono::steady_clock::now() - reference_ < kMinTime) {
+            // 1マイクロ秒スリープ
+            std::this_thread::sleep_for(std::chrono::microseconds(1));
+        }
+    }
+    // 現在の時間を記録する
+    reference_ = std::chrono::steady_clock::now();
+}
