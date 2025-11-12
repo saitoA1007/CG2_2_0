7e7c65590a039f71dcc541bb61d07ae6d3023d3a
saitoA1007 <k024g0004@g.neec.ac.jp>
2025-11-12 11:11:40 +0900

    DirectXのデバックレイヤー機能を分解した

--- Project/EngineSource/Graphics/DirectX/DirectXCommon.cpp
+++ Project/EngineSource/Graphics/DirectX/DirectXCommon.cpp
@@ -22,7 +22,8 @@ void DirectXCommon::Initialize(HWND hwnd, uint32_t width, uint32_t height, SrvMa
     device_->Initialize();
 
 #ifdef _DEBUG
-    DebugLayer(device_->GetDevice());
+    debugger_ = std::make_unique<DXDebugger>();
+    debugger_->InitializeDebugLayer(device_->GetDevice());
 #endif
 
     command_ = std::make_unique<DXCommand>();
@@ -147,38 +148,6 @@ void DirectXCommon::PostDraw(ImGuiManager* imGuiManager)
     // 次のフレーム用にコマンドリストを準備
     command_->Reset();
 }
-
-#ifdef _DEBUG
-void DirectXCommon::DebugLayer(ID3D12Device* device) {
-
-    // InfoQueueによるエラーチェックの設定
-    ComPtr<ID3D12InfoQueue> infoQueue = nullptr;
-    if (SUCCEEDED(device->QueryInterface(IID_PPV_ARGS(&infoQueue)))) {
-        // ヤバいエラー時に止まる
-        infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_CORRUPTION, true);
-        // エラー時に止まる
-        infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_ERROR, true);
-
-        // 警告時に止まる
-        // infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_WARNING, true);
-
-        // 抑制するメッセージのID
-        D3D12_MESSAGE_ID denyIds[] = {
-            // Window11でのDXGIデバッカーとDX12デバックレイヤーの相互作用バグによるエラーメッセージ
-//			// https://stackoverflow.com/questions/69885245/directx-12-application-is-crashing-in-windows-11
-            D3D12_MESSAGE_ID_RESOURCE_BARRIER_MISMATCHING_COMMAND_LIST_TYPE
-        };
-        D3D12_MESSAGE_SEVERITY severities[] = { D3D12_MESSAGE_SEVERITY_INFO };
-        D3D12_INFO_QUEUE_FILTER filter{};
-        filter.DenyList.NumIDs = _countof(denyIds);
-        filter.DenyList.pIDList = denyIds;
-        filter.DenyList.NumSeverities = _countof(severities);
-        filter.DenyList.pSeverityList = severities;
-        // 指定したメッセージの表示を抑制する
-        infoQueue->PushStorageFilter(&filter);
-    }
-}
-#endif
 
 void DirectXCommon::InitializeFixFPS() {
     // 現在の時間を記録する
