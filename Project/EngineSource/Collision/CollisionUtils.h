3fd556ad67a5cb78e7c509651793bcd3ca5dbf6d
saitoA1007 <k024g0004@g.neec.ac.jp>
2025-11-16 14:24:34 +0900

    当たり判定のシステムを改善

--- Project/EngineSource/Collision/CollisionUtils.h
+++ Project/EngineSource/Collision/CollisionUtils.h
@@ -1,32 +1,33 @@
 #pragma once
 #include"Geometry.h"
+#include"CollisionResult.h"
 
 namespace GameEngine {
 
 	// 球同士の衝突判定
-	bool IsSpheresCollision(const Sphere& s1, const Sphere& s2);
+	CollisionResult IsSpheresCollision(const Sphere& s1, const Sphere& s2);
 
 	// 平面と球の衝突判定
-	bool IsSpherePlaneCollision(const Sphere& sphere, const Plane& plane);
+	CollisionResult IsSpherePlaneCollision(const Sphere& sphere, const Plane& plane);
 
 	// 線と平面の衝突判定
-	bool IsSegmentPlaneCollision(const Segment& segment, const Plane& plane);
+	CollisionResult IsSegmentPlaneCollision(const Segment& segment, const Plane& plane);
 
 	// 線と三角形の衝突判定
-	bool IsSegmentTriangleCollision(const Triangle& triangle, const Segment& segment);
+	CollisionResult IsSegmentTriangleCollision(const Triangle& triangle, const Segment& segment);
 
 	// aabb同士の衝突判定
-	bool IsAABBCollision(const AABB& aabb1, const AABB& aabb2);
+	CollisionResult IsAABBCollision(const AABB& aabb1, const AABB& aabb2);
 
 	// aabbと球の衝突判定
-	bool IsAABBSphereCollision(const AABB& aabb, const Sphere& sphere);
+	CollisionResult IsAABBSphereCollision(const AABB& aabb, const Sphere& sphere);
 
 	// aabbと線の衝突判定
-	bool IsAABBSegmentCollision(const AABB& aabb, const Segment& segment);
+	CollisionResult IsAABBSegmentCollision(const AABB& aabb, const Segment& segment);
 
 	// obbと球の衝突判定
-	bool IsOBBSphereCollision(const OBB& obb, const Sphere& sphere);
+	CollisionResult IsOBBSphereCollision(const OBB& obb, const Sphere& sphere);
 
 	// obbと線の衝突判定
-	bool IsOBBSegmentCollision(const OBB& obb, const Segment& segment);
+	CollisionResult IsOBBSegmentCollision(const OBB& obb, const Segment& segment);
 }
