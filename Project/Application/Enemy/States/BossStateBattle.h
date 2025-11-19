5627e7443b1b257dc5a0176b8220fe3c2f633aff
saitoA1007 <k024g0004@g.neec.ac.jp>
2025-11-20 00:10:35 +0900

    ボスの突進攻撃を作成した

--- Project/Application/Enemy/States/BossStateBattle.h
+++ Project/Application/Enemy/States/BossStateBattle.h
@@ -13,16 +13,27 @@ public:
 		Normal, // 通常状態
 		RushAttack, // 突進攻撃
 		ShotAttack, // 弾の発射
-		FallAttack, // 氷柱を落とす攻撃
+		IceFallAttack, // 氷柱を落とす攻撃
 
 		MaxCount // 状態の数
 	};
 
 public:
-
 	BossStateBattle(BossContext& context,const float& stageRadius);
+
+	/// <summary>
+	/// 入りの処理
+	/// </summary>
 	void Enter() override;
+
+	/// <summary>
+	/// 更新処理
+	/// </summary>
 	void Update() override;
+
+	/// <summary>
+	/// 終わりの処理
+	/// </summary>
 	void Exit() override;
 
 private:
@@ -31,7 +42,7 @@ private:
 	BossContext& bossContext_;
 
 	// ボスの振る舞い
-	ButtleBehavior behavior_ = ButtleBehavior::Normal;
+	ButtleBehavior currentBehavior_ = ButtleBehavior::Normal;
 	// 振る舞いの変更を管理
 	std::optional<ButtleBehavior> behaviorRequest_ = std::nullopt;
 	// ボスの状態テーブル
@@ -39,8 +50,93 @@ private:
 	// ボスが指定した状態を行うためのリセット処理
 	std::array<std::function<void()>, static_cast<size_t>(ButtleBehavior::MaxCount)> resetBehaviorParamTable_;
 	
+	// 現在の状態が有効化管理
+	bool isCurrentBehaviorActive_ = false;
+
 	// ステージの半径
 	float stageRadius_ = 0.0f;
+
+	// 大きさ
+	Vector3 size_ = { 1.0f,1.0f,1.0f };
+
+private:
+
+	// Behavior : Normal
+	// 行動をおこなうための繋ぎの行動に使用する変数============
+
+	// 元の場所に戻る位置
+	Vector3 startBackPos_ = {};
+	Vector3 endBackPos_ = {};
+
+	// 元の場所に戻る時間
+	float backTimer_ = 0.0f;
+	float backMaxTime_ = 1.0f;
+
+	// 普段いる高さ
+	float defalutPosY_ = 5.0f;
+
+	// 突進に使う変数======================
+
+	// 回転の時に移動する速度
+	float rotSpeed_ = 20.0f;
+
+	// 回転する時間
+	float rotMoveTimer_ = 0.0f;
+	float rotMaxMoveTime_ = 0.0f;
+	// 移動する角度
+	float startAngle_ = 0.0f;
+	float endAngle_ = 0.0f;
+
+	// 突進する時間
+	float rushTimer_ = 0.0f;
+	float fallTimer_ = 0.0f;
+	float rushMaxTime_ = 2.0f;
+
+	// 突進する位置
+	Vector3 startRushPos_ = {};
+	Vector3 endRushPos_ = {};
+	// 突進の終わる位置の距離をどれだけ伸ばすか
+	float offsetEndRush_ = 5.0f;
+
+	bool isRotMove_ = true;
+
+
+private: // 各振る舞いの処理
+
+	/// <summary>
+	/// 振る舞いを管理
+	/// </summary>
+	void ControllBehavior();
+
+	/// <summary>
+	/// 通常行動のリセット処理
+	/// </summary>
+	void ResetNormal();
+
+	/// <summary>
+	/// 通常の行動処理
+	/// </summary>
+	void NormalUpdate();
+
+	/// <summary>
+	/// 突進攻撃のリセット処理
+	/// </summary>
+	void ResetRush();
+
+	/// <summary>
+	/// 突進の攻撃処理
+	/// </summary>
+	void RushAttackUpdate();
+
+	/// <summary>
+	/// 弾を発射する攻撃処理
+	/// </summary>
+	void ShotAttackUpdate();
+
+	/// <summary>
+	/// 氷柱を落とす攻撃処理
+	/// </summary>
+	void IceFallAttackUpdate();
 
 private:
 
@@ -54,3 +150,18 @@ private:
 	/// </summary>
 	void ApplyDebugParam();
 };
+
+// ヘルプ関数
+namespace {
+
+	// 最短経路で角度を補間する
+	float LerpShortAngle(float a, float b, float t);
+
+	// 補間した差分を求める
+	float GetAngleDiff(float a, float b);
+
+	// 距離に応じた時間を求める
+	float GetMoveTimeDistance(float startAngle, float endAngle, float radius, float speed);
+
+	float EaseOutQuart(float t);
+}
