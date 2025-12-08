6d05238103589f7c1f4a93808fa303c52c4f5d95
saitoA1007 <k024g0004@g.neec.ac.jp>
2025-12-08 11:02:38 +0900

    ボスの入り演出(仮)を追加

--- Project/Application/Enemy/States/BossStateIn.cpp
+++ Project/Application/Enemy/States/BossStateIn.cpp
@@ -1,6 +1,9 @@
 #include"BossStateIn.h"
 #include"Application/Enemy/BossState.h"
 #include"FPSCounter.h"
+#include"GameParamEditor.h"
+#include"EasingManager.h"
+#include<numbers>
 using namespace GameEngine;
 
 BossStateIn::BossStateIn(BossContext& context) : bossContext_(context) {
@@ -17,9 +20,14 @@ BossStateIn::BossStateIn(BossContext& context) : bossContext_(context) {
 
 void BossStateIn::Enter() {
 	// 自分の初期位置を設定
-	bossContext_.worldTransform->transform_.translate.x = 10.0f;
-	bossContext_.worldTransform->transform_.translate.z = 10.0f;
-	bossContext_.worldTransform->transform_.translate.y = 20.0f;
+	bossContext_.worldTransform->transform_.translate = startPos_;
+
+	// 正面を向くように
+	bossContext_.worldTransform->transform_.rotate.y = std::numbers::pi_v<float>;
+
+	bossContext_.animator_->SetAnimationData(&(*bossContext_.animationData_)[static_cast<size_t>(enemyAnimationType::BaseMove)]["基本移動"]);
+	bossContext_.animationTimer = 0.0f;
+
 }
 
 void BossStateIn::Update() {
@@ -28,13 +36,49 @@ void BossStateIn::Update() {
 	ApplyDebugParam();
 #endif
 
-	// 落下する
-	bossContext_.worldTransform->transform_.translate.y -= 10.0f * FpsCounter::deltaTime;
+	if (isMove_) {
 
-	// 地面に付いたらバトル状態へ変更する
-	if (bossContext_.worldTransform->transform_.translate.y <= 6.0f) {
-		bossContext_.bossStateRequest_ = BossState::Battle;
-	}
+		timer_ += FpsCounter::deltaTime / InTime_;
+
+		// 縦移動
+		float posY = 0.0f;
+		float totalCycle = timer_ * static_cast<float>(cycleCount_);
+		float localTimer = std::fmodf(totalCycle, 1.0f);
+
+		if (localTimer <= 0.5f) {
+			float t = localTimer / 0.5f;
+			posY = Lerp(0.0f, moveHeight_, EaseInOut(t));
+		} else {
+			float t = (localTimer - 0.5f) / 0.5f;
+			posY = Lerp(moveHeight_, 0.0f, EaseInOut(t));
+		}
+
+		bossContext_.worldTransform->transform_.translate = Lerp(startPos_, endPos_, EaseInOut(timer_));
+		bossContext_.worldTransform->transform_.translate.y += posY;
+
+		// アニメーション
+		bossContext_.animationTimer += FpsCounter::deltaTime;
+		if (bossContext_.animationTimer >= 1.0f) {
+			bossContext_.animationTimer = 0.0f;
+		}
+
+		// 地面に付いたらバトル状態へ変更する
+		if (timer_ >= 1.0f) {
+			bossContext_.animator_->SetAnimationData(&(*bossContext_.animationData_)[static_cast<size_t>(enemyAnimationType::Scream)]["Scream"]);
+			isMove_ = false;
+			timer_ = 0.0f;
+		}
+	} else {
+
+		timer_ += FpsCounter::deltaTime / waitTime_;
+
+		// アニメーション
+		bossContext_.animationTimer = timer_;
+
+		if(timer_ >= 1.0f){
+			bossContext_.bossStateRequest_ = BossState::Battle;
+		}
+	}	
 }
 
 void BossStateIn::Exit() {
@@ -42,9 +86,19 @@ void BossStateIn::Exit() {
 }
 
 void BossStateIn::RegisterBebugParam() {
-
+	GameParamEditor::GetInstance()->AddItem(kGroupName, "StartPos", startPos_);
+	GameParamEditor::GetInstance()->AddItem(kGroupName, "EndPos", endPos_);
+	GameParamEditor::GetInstance()->AddItem(kGroupName, "InTime", InTime_);
+	GameParamEditor::GetInstance()->AddItem(kGroupName, "WaitTime", waitTime_);
+	GameParamEditor::GetInstance()->AddItem(kGroupName, "MoveHeight", moveHeight_);
+	GameParamEditor::GetInstance()->AddItem(kGroupName, "CycleCount", cycleCount_);
 }
 
 void BossStateIn::ApplyDebugParam() {
-
+	startPos_ = GameParamEditor::GetInstance()->GetValue<Vector3>(kGroupName, "StartPos");
+	endPos_ = GameParamEditor::GetInstance()->GetValue<Vector3>(kGroupName, "EndPos");
+	InTime_ = GameParamEditor::GetInstance()->GetValue<float>(kGroupName, "InTime");
+	waitTime_ = GameParamEditor::GetInstance()->GetValue<float>(kGroupName, "WaitTime");
+	moveHeight_ = GameParamEditor::GetInstance()->GetValue<float>(kGroupName, "MoveHeight");
+	cycleCount_ = GameParamEditor::GetInstance()->GetValue<uint32_t>(kGroupName, "CycleCount");
 }
--- Project/Application/Enemy/States/BossStateIn.h
+++ Project/Application/Enemy/States/BossStateIn.h
@@ -25,6 +25,20 @@ private:
     // ボスの共通データを取得
     BossContext& bossContext_;
 
+
+	std::string kGroupName = "BossInMove";
+	Vector3 startPos_ = {0.0f,0.0f,0.0f};
+	Vector3 endPos_ = {0.0f,15.0f,0.0f};
+	float moveHeight_ = 4.0f;
+	uint32_t cycleCount_ = 3;
+
+	float timer_ = 0.0f;
+
+	float InTime_ = 2.0f;
+	float waitTime_ = 2.0f;
+
+	bool isMove_ = true;
+
 private:
 
 	/// <summary>
