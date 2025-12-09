be15ea00fe2f282cecc99f777197a4d0a2e6a87d
鈴木 色音 <k024g0058@g.neec.ac.jp>
2025-12-09 17:16:32 +0900

    プレイヤー急降下攻撃時のエフェクトの調整

--- Project/Application/Player/Effect/PlayerAttackDownEffect.cpp
+++ Project/Application/Player/Effect/PlayerAttackDownEffect.cpp
@@ -5,7 +5,7 @@ void PlayerAttackDownEffect::Initialize() {
     isActive_ = true;
     const float kRotate90 = static_cast<float>(M_PI) / 2.0f;
     for (auto &wt : worldTransforms_) {
-        wt.Initialize({ {2.0f,2.0f,2.0f}, {kRotate90,0.0f,0.0f}, {0.0f,0.0f,0.0f} });
+        wt.Initialize({ {1.5f,1.5f,10.0f}, {kRotate90,0.0f,0.0f}, {0.0f,1.0f,0.0f} });
     }
 }
 
